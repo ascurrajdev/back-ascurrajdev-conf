c12c58620ac4e4be0e24cf11460c823e92c537d9
Jose Ascurra <info@ascurrajdev.xyz>
2022-02-04 20:13:22 -0300

    Cambios en la configuracion de la conexion a websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,9 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http'
+                'host' => env("BROADCASTING_HOST"),
+                'port' => env("BROADCASTING_PORT"),
+                'useTLS' => true
             ],
         ],
 
